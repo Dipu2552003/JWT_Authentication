c314487ec1a1cdea2e566704dc132423dc5d87d1
dipkul <dipkul.khandelwal21@vit.edu>
2024-07-18 09:04:55 +0530

    frontend comments

--- Index.js
+++ Index.js
@@ -33,3 +33,28 @@ app.use("/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// // **************frontend**********************************
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// <BrowserRouter>
+// <Routes>
+//   <Route exact path="/" element={<Homepage />} />
+//   <Route exact path="/home" element={<Homepage />} />
+//   <Route path="/question/:question_id" element={<Question />} />
+//   <Route path="/tag/:tag" element={<Tags />} />
+//   <Route path="/user/" element={<UserProfile />} />
+// </Routes>
+// </BrowserRouter>
+
+// // **************Homepage**********************************
+
+// const [cookies, setCookie] = useCookies(["jwttokenloginuser"]);
+
+// useEffect(() => {
+//   // Initialize state from cookies
+//   const jwtTokenLoginUserCookie = cookies.jwttokenloginuser || "";
+//   // Additional initialization logic if needed
+// }, [cookies.jwttokenloginuser]);
+
+// if (cookies.jwttokenloginuser){}
